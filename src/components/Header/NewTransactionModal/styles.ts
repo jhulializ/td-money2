19b5ad2f35f0be7cca2fe3c0aae60338e40c0951
Jhulia Liz <jhulializ360@gmail.com>
2023-08-03 15:52:47 -0300

    button acessével

--- src/components/Header/NewTransactionModal/styles.ts
+++ src/components/Header/NewTransactionModal/styles.ts
@@ -68,7 +68,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -76,11 +76,11 @@ export const TransactionType = styled.div`
 `;
 
 interface TransactionTypeButtonProps {
-  variant: 'income' | 'outcome';
+  variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
-  background: ${props => props.theme["gray-700"]};
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
   align-items: center;
@@ -89,9 +89,29 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   border-radius: 6px;
   cursor: pointer;
   border: 0;
-  color: ${props => props.theme["gray-300"]};
-  
+  color: ${(props) => props.theme["gray-300"]};
+
   svg {
-    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme["gray-600"]};
+  }
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `;

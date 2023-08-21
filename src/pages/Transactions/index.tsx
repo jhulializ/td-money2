7011198b19b99de750a8a1be3f6e6f13df99a86b
Jhulia Liz <jhulializ360@gmail.com>
2023-08-21 12:44:07 -0300

    useCallBack

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContextSelector } from 'use-context-selector'
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Header/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
@@ -8,7 +8,9 @@ import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styl
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+   const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>

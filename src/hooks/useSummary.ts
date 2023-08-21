2f32a54f1ad8840f72e209be1d00a5dce069ca0c
Jhulia Liz <jhulializ360@gmail.com>
2023-08-21 17:25:52 -0300

    UseMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,20 +1,22 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from "../contexts/TransactionsContext";
+import {useMemo} from 'react'
 
 export function useSummary() {
     const transactions = useContextSelector(TransactionsContext, (context) => {
         return context.transactions
       })
 
-    const summary = transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === 'income') {
-          acc.income += transaction.price;
-          acc.total += transaction.price;
-        } else {
-          acc.outcome += transaction.price;
-          acc.total -= transaction.price;
-        }
+      const summary = useMemo(() => {
+        return transactions.reduce(
+          (acc, transaction) => {
+            if (transaction.type === 'income') {
+              acc.income += transaction.price
+              acc.total += transaction.price
+            } else {
+              acc.outcome += transaction.price
+              acc.total -= transaction.price
+            }
   
         return acc;
       },
@@ -24,6 +26,7 @@ export function useSummary() {
         total: 0,
       },
     )
+}, [transactions])
 
     return summary;
 }
